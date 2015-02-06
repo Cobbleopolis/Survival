34ed94b6d86a5463b2f21bc80802c165059bb8f2
Cobbleopolis <cobbleopolis@gmail.com>
2015-02-06 10:18:01 -0500

    Started on layout.

--- html/scripts/index.js
+++ html/scripts/index.js
@@ -1,4 +1,20 @@
-$(window).on("load", function(){
-    console.log("Load");
-    $(".profile-photo").hide();
+var supplies = [
+    {
+        name: "REI Durable Stuff Sack",
+        price: "$7.50 – $11.50",
+        link: "http://www.rei.com/product/862591/rei-durable-stuff-sack"
+    }
+];
+
+$(window).on("load", function () {
+    //console.log("Load");
+    createSupplies();
 });
+
+function createSupplies() {
+    html = '';
+    for (var i = 0; i < supplies.length; i++) {
+        html += '<div class="ui top attached segment">';
+        html += '<p>' + supplies[i].name + '</p>';
+    }
+}
--- node.js
+++ node.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res){
 
 app.use(express.static(__dirname + '/html'));
 
-var server = app.listen(8080, function () {
+var server = app.listen(80, function () {
 
     var host = server.address().address;
     var port = server.address().port;
