92bc2fa0c1cb10fdb2cd90f903e6bac949300ce9
Cobbleopolis <agounimon@yahoo.com>
2015-02-06 19:41:32 -0500

    Worked on weather and supplies.

--- html/scripts/index.js
+++ html/scripts/index.js
@@ -1,20 +1,26 @@
 var supplies = [
     {
-        name: "REI Durable Stuff Sack",
-        price: "$7.50 – $11.50",
-        link: "http://www.rei.com/product/862591/rei-durable-stuff-sack"
+        name: "VAUDE Sioux 400 Sleeping Bag - 2013 Closeout",
+        price: "$59.73",
+        desc: "This warm, synthetic-insulation sleeping bag has a trim mummy cut that helps retain heat close to your body, right where you need it. It makes a great choice for summertime camping in mild climates.",
+        link: "http://www.rei.com/product/884219/vaude-sioux-400-sleeping-bag-2013-closeout"
     }
 ];
 
 $(window).on("load", function () {
-    //console.log("Load");
+    $("#map").width($("#map").parent().width());
     createSupplies();
 });
 
 function createSupplies() {
     html = '';
     for (var i = 0; i < supplies.length; i++) {
-        html += '<div class="ui top attached segment">';
-        html += '<p>' + supplies[i].name + '</p>';
+        html += '<div class="ui segment">';
+        html += '<p><strong>' + supplies[i].name + '</strong></p>';
+        html += '<div class="ui fitted divider"></div>';
+        html += '<p>Description: ' + supplies[i].desc + '</p>';
+        html += '<p>Price: ' + supplies[i].price + '</p>';
+        html += '</div>';
     }
+    $("#suppliesContent").html(html);
 }
