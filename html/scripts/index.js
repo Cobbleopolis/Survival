3334ca5661832fa3aad6201971d5227620312641
Cobbleopolis <agounimon@yahoo.com>
2015-02-06 22:09:01 -0500

    Did some work on Supplies and did some reformatting on Weather

--- html/scripts/index.js
+++ html/scripts/index.js
@@ -3,13 +3,22 @@ var supplies = [
         name: "VAUDE Sioux 400 Sleeping Bag - 2013 Closeout",
         price: 59.73,
         desc: "This warm, synthetic-insulation sleeping bag has a trim mummy cut that helps retain heat close to your body, right where you need it. It makes a great choice for summertime camping in mild climates.",
-        link: "http://www.rei.com/product/884219/vaude-sioux-400-sleeping-bag-2013-closeout"
+        link: "http://www.rei.com/product/884219/vaude-sioux-400-sleeping-bag-2013-closeout",
+        linkName: "REI"
     },
     {
         name: "REI Camper First-Aid Kit",
         price: 79.50,
         desc: "A handy essential you can stow inside a vehicle, the REI Camper First-Aid Kit provides medical supplies for groups and families.",
-        link: "http://www.rei.com/product/867434/rei-camper-first-aid-kit"
+        link: "http://www.rei.com/product/867434/rei-camper-first-aid-kit",
+        linkName: "REI"
+    },
+    {
+        name: "KOR Nava BPA Free 650ml Filter Water Bottle",
+        price: 21.95,
+        desc: "See site for details.",
+        link: "http://www.amazon.com/KOR-650ml-Filter-Water-Bottle/dp/B00FUANGRY/ref=sr_1_1?ie=UTF8&qid=1423276341&sr=8-1&keywords=KOR+Nava+BPA+Free+650ml+Filter+Water+Bottle%2C+White%2FBlue",
+        linkName: "Amazon"
     }
 ];
 
@@ -20,13 +29,21 @@ $(window).on("load", function () {
 
 function createSupplies() {
     html = '';
+    cost = 0;
     for (var i = 0; i < supplies.length; i++) {
         html += '<div class="ui segment">';
         html += '<p><strong>' + supplies[i].name + '</strong></p>';
-        html += '<div class="ui fitted divider"></div>';
-        html += '<p>Description: ' + supplies[i].desc + '</p>';
-        html += '<p>Price: $' + supplies[i].price + '</p>';
+        html += '<div class="ui fitted inverted divider"></div>';
+        html += '<p class="suppliesDesc">Description: ' + supplies[i].desc + '</p>';
+        html += '<p class="suppliesPrice">Price: $' + supplies[i].price + '</p>';
+        html += '<div class="ui fitted inverted divider"></div>';
+        html += '<p class="suppliesPrice">Buy/View: <a href="' + supplies[i].link + '">' + supplies[i].linkName + '</a></p>';
         html += '</div>';
+        cost += supplies[i].price;
+        //console.log(cost);
     }
+    html += '<div class="ui fitted inverted divider"></div>';
+    html += '<p class="suppliesTotalPrice">Total Price: $' + Math.round(cost * 100) / 100 + '</p>';
+
     $("#suppliesContent").html(html);
 }
